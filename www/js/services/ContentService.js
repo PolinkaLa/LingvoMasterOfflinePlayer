46f7b74bfa27f072adcfd35322f9a19ddbe9ae31
PolinkaLa <miss.lappo@yandex.ru>
2016-03-22 00:16:03 +0300

    update ContentService and ExerciseService

--- www/js/services/ContentService.js
+++ www/js/services/ContentService.js
@@ -1,4 +1,13 @@
 function ContentService () {
-
+	
+	/**
+	 * function to get list paths to the files of course in the file system
+	 * @param  {number} idCourse - the unique number of the course
+	 * @return {array[string]} - list paths to the file of course
+	 */
+	this.getListContent = function (idCourse) {
+		var listUrlFiles = [];
+		return listUrlFiles;
+	}
 };
 
