94daae005ca1d198b640201ea966330b690741f7
PolinkaLa <miss.lappo@yandex.ru>
2016-03-24 13:16:40 +0300

    add new services

---
+++ www/js/services/LoadService.js
@@ -0,0 +1,12 @@
+function LoadService () {
+
+	/**
+	 * function to download archive with course on device
+	 * @param  {string} urlArchive - link to download archive with course
+	 * @return {string} - path to dowloaded archive in the file system
+	 */
+	this.download = function(urlArchive) {
+		var pathArchive = "";
+		return pathArchive;
+	}
+}
